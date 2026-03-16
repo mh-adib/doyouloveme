4d695136eca38e50a84b1e88ddb99649c7111cad
Md Mazharul Haque <mdmazharulhaqueadib@gmail.com>
2026-03-16 19:26:47 +0600

    added a new file

--- do_you_love_me.js
+++ do_you_love_me.js
@@ -1,20 +1,17 @@
 const questionContainer = document.querySelector(".question-container");
-const resultContainer = document.querySelector(".result-container.yes");
+const resultContainer = document.querySelector(".result-container");
 const gifResult = document.querySelector(".gif-result");
 const heartLoader = document.querySelector(".cssload-main");
 const yesBtn = document.querySelector(".js-yes-btn");
 const noBtn = document.querySelector(".js-no-btn");
 
-// /change the postion of no button
 noBtn.addEventListener("mouseover", () => {
   const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
-  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetHeight);
 
   noBtn.style.left = `${newX}px`;
   noBtn.style.top = `${newY}px`;
 });
-
-// yes button functionality
 
 yesBtn.addEventListener("click", () => {
   questionContainer.style.display = "none";
